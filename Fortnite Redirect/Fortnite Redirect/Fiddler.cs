41d6fe6dd275b225c1ec734fcb09d1a295d449a4
Birulol <150455195+notbirufn@users.noreply.github.com>
2025-02-23 21:46:47 +0900

    20250223

---
+++ Fortnite Redirect/Fortnite Redirect/Fiddler.cs
@@ -0,0 +1,17 @@
+﻿using Fiddler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortnite_Redirect
+{
+    internal class Fiddler
+    {
+        public static bool Setup()
+        {
+            return CertMaker.createRootCert() && CertMaker.trustRootCert();
+        }
+    }
+}
--- Fortnite Redirect/Fortnite Redirect/Program.cs
+++ Fortnite Redirect/Fortnite Redirect/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fiddler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,41 @@ namespace Fortnite_Redirect
     {
         static void Main(string[] args)
         {
+            if (Fiddler.Setup())
+            {
+                FiddlerCoreStartupSettings startupSettings = new FiddlerCoreStartupSettingsBuilder().ListenOnPort(9999).DecryptSSL().RegisterAsSystemProxy().Build();
+
+                FiddlerApplication.BeforeRequest += OnBeforeRequest;
+                FiddlerApplication.BeforeResponse += OnBeforeResponse;
+
+                FiddlerApplication.Startup(startupSettings);
+
+                Console.ReadKey(true);
+
+                FiddlerApplication.Shutdown();
+            }
+        }
+        private static void OnBeforeRequest(Session session)
+        {
+            if (session.RequestHeaders["User-Agent"].Split('/')[0] == "FortniteGame" || session.RequestHeaders["User-Agent"].Split('/')[0] == "Fortnite" || session.RequestHeaders["User-Agent"].Split('/')[0] == "EOS-SDK")
+            {
+                if (session.HTTPMethodIs("CONNECT"))
+                {
+                    session["x-replywithtunnel"] = "FortniteTunnel";
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("LogURL: " + session.fullUrl);
+                }
+
+                session.fullUrl = "http://localhost:1911" + session.PathAndQuery;
+            }
+        }
+
+        private static void OnBeforeResponse(Session session)
+        {
+
         }
     }
 }
